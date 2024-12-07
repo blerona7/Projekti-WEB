b61d5bbc9329c55ff3e02a03083c42bf6219a99d
Blerona <ahmetiblerona@gmail.com>
2024-12-08 00:14:02 +0100

    ndryshime

---
+++ js/login.js
@@ -0,0 +1,18 @@
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+      if (username == "") {
+        alert("Ju lutem shkruani username-n tuaj!");
+        return false;
+      } else if (username.length < 3) {
+        alert("Username duhet te jete i gjate te pakten 3 karaktere!");
+        return false;
+      } else if (password == "") {
+        alert("Ju lutem shkruani fjalekalimin tuaj");
+        return false;
+      } else {
+        alert("Kyqja juaj ishte e suksesshme");
+        window.location = "ReadMore1.html";
+      }
+    }
