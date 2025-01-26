dc960f236b8b6e4bc2df8af068ef6018c034cc3c
Blerona <ahmetiblerona@gmail.com>
2025-01-26 20:03:11 +0100

    final changes

--- HTMLfiles/Homepage.php
+++ HTMLfiles/Homepage.php
@@ -102,8 +102,8 @@ $articles=$mapper->getAllArticles();
                                 <div class="hot-topic-content">
                                     <h2>Kush do te perfaqesoje Shqiperine ne Eurovision 2024? </h2>
 
-                                    <h3>Eurovision 2025</h3>
-                                    <p>Kush eshte Elvana Gjata, qe rrembeu vendin e dyte ne Eurovision 2024.</p>
+                                    <h3>Eurovision 2024</h3>
+                                    <p>Kush eshte Albina Kelmendi, qe rrembeu vendin e dyte dhe te pare ne Eurovision 2023.</p>
                                     <a href="Readmore.php">Lexo</a>
                                 </div>
                             </div>
@@ -227,6 +227,44 @@ $articles=$mapper->getAllArticles();
         <?php
         include('../Re-Usable/footer.php');
         ?>
-    </body>
+    <!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
 
 </html>
